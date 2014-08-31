69e6ef2b7e2fe94dc6dee8f327c39f7b4e03524c
nekdenis <batman36@mail.ru>
2014-08-31 14:48:15 +0400

    Initial commit. Added base structure

---
+++ app/src/main/java/com/github/nekdenis/simpledict/fragment/WordListFragment.java
@@ -0,0 +1,22 @@
+package com.github.nekdenis.simpledict.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.github.nekdenis.simpledict.R;
+
+public class WordListFragment extends Fragment {
+
+        public WordListFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
