aa9884990d3f7dd791fd20286e2893607423294e
nekdenis <batman36@mail.ru>
2014-08-31 20:17:13 +0400

    Added layouts

--- app/src/main/java/com/github/nekdenis/simpledict/fragment/WordListFragment.java
+++ app/src/main/java/com/github/nekdenis/simpledict/fragment/WordListFragment.java
@@ -16,7 +16,7 @@ public class WordListFragment extends Fragment {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_word_list, container, false);
             return rootView;
         }
     }
